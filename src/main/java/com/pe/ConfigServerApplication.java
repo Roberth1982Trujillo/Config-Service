6dfbe1521675975ef942736363e281d9f1fc2caa
Roberth1982Trujillo <roberto.rodriguez.sanchez.1982@gmail.com>
2026-09-06 21:24:20 -0500

    Reemplazar todo el proyecto remoto con version local

---
+++ src/main/java/com/pe/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+		System.out.println("Iniciado: Config Service");
+	}
+
+}
